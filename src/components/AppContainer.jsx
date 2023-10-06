95e583b43dc3a2aef1de3ef791b5cb1baa03743f
Vitalii Sili <vitaliisili@yahoo.com>
2023-10-06 15:54:26 +0200

    Fix scrollbar overflow

--- src/components/AppContainer.jsx
+++ src/components/AppContainer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const AppContainer = ({children}) => {
     return (
-        <div className='flex bg-[#1a1a1a] justify-center overflow-scroll'>
+        <div className='flex bg-[#1a1a1a] justify-center overflow-auto'>
             {children}
         </div>
     )
